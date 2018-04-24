b930f1e2e1400c02c154d3ac86697a3e709d8f48
Julian Coleman <imago.dei26@gmail.com>
2018-04-23 21:26:53 -0700

    extend description of #validate

--- lib/validate/validate.ts
+++ lib/validate/validate.ts
@@ -5,8 +5,10 @@ import { isString } from "../isString";
  * Validate
  *
  * _Accepts an argument and a callback. If the argument is
- * a string, the callback is fired, otherwise an
- * `OrchestraTypeError` is thrown_
+ * a string, the callback is called with the value as its
+ * argument, otherwise an `OrchestraTypeError` is thrown.
+ * If no value or callback are provided, an
+ * `OrchestraReferenceError` is thrown._
  */
 
 
