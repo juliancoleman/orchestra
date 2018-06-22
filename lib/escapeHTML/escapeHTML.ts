f48a34c36e4b927ff0a3e709b79302048256163d
Julian Coleman <imago.dei26@gmail.com>
2018-06-21 23:34:15 -0700

    update escapeHTML description doc

--- lib/escapeHTML/escapeHTML.ts
+++ lib/escapeHTML/escapeHTML.ts
@@ -1,3 +1,5 @@
+// tslint:disable max-line-length
+
 import { coerceString } from "../";
 import { escapeCharacters } from "../../helpers/escapeCharacters";
 import { escape } from "../../helpers/regex";
@@ -9,7 +11,7 @@ import { escape } from "../../helpers/regex";
  * @param {string} str
  * @returns {string}
  *
- * @description _Converts an HTML node to a safe string._
+ * @description _Converts HTML special characters to their entity equivalents. Cannot convert nodes returned by `querySelector`._
  * @since v0.0.1
  */
 
