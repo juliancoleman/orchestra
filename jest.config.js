6ca32987beb7fc114b2ea6a9c6383194bccfae5a
Julian Coleman <jcoleman@techempower.com>
2019-01-30 11:25:20 -0800

    Moves everything to src/

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
   ],
   coverageDirectory: "coverage",
   testMatch: [
-    "<rootDir>/lib/**/?(*.)(spec|test).(js|ts)?(x)",
-    "<rootDir>/helpers/**/?(*.)(spec|test).(js|ts)?(x)",
+    "<rootDir>/src/lib/**/?(*.)(spec|test).(js|ts)?(x)",
+    "<rootDir>/src/helpers/**/?(*.)(spec|test).(js|ts)?(x)",
   ],
   reporters: process.env.CI ? undefined : ["jest-spec-reporter"],
   rootDir: ".",
