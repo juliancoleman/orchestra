e98aba5c3a029b0fd98dfb1890540eb0f5a84b6e
Julian Coleman <jcoleman.engineering@gmail.com>
2019-06-04 02:23:02 -0700

    Remove unneeded files

--- src/helpers/regex/index.ts
+++ src/helpers/regex/index.ts
@@ -2,8 +2,5 @@ export * from "./entity";
 export * from "./entityCode";
 export * from "./entityNumber";
 export * from "./escape";
-export * from "./firstCharInSentenceIsLower";
-// export * from "./lookAheadCapitalLetters";
 export * from "./underscores";
 export * from "./whitespaces";
-// export * from "./wud";
