0e48f3510000d34138a3847ff49c07c0d4d343e4
Julian Coleman <jcoleman.engineering@gmail.com>
2019-06-03 20:54:19 -0700

    Adds pascalize function to library

--- src/lib/pascalize/pascalize.spec.ts
+++ src/lib/pascalize/pascalize.spec.ts
@@ -1,5 +1,75 @@
-describe("#pascalize", () => {
-  it("test not written", () => {
-    return;
+import { pascalize } from "./pascalize";
+
+// tslint:disable-next-line: variable-name
+const MozTransform = "MozTransform";
+
+describe("#pascalize()", () => {
+  it("returns a string", () => {
+    const result = pascalize("-moz-transform");
+
+    expect(typeof result).toBe("string");
+  });
+
+  it("converts lowercase", () => {
+    const result = pascalize("moz transform");
+
+    expect(result).toBe(MozTransform);
+  });
+
+  it("converts uppercase", () => {
+    const result = pascalize("MOZ TRANSFORM");
+
+    expect(result).toBe(MozTransform);
+  });
+
+  it("converts camelCase", () => {
+    // This test is dumb, I know.
+    const result = pascalize("mozTransform");
+
+    expect(result).toBe(MozTransform);
+  });
+  it("converts snake_case", () => {
+    const result = pascalize("moz_transform");
+
+    expect(result).toBe(MozTransform);
+  });
+  it("converts PascalCase", () => {
+    const result = pascalize("MozTransform");
+
+    expect(result).toBe(MozTransform);
+  });
+  it("converts kebab-case", () => {
+    const result = pascalize("moz-transform");
+
+    expect(result).toBe(MozTransform);
+  });
+  it("converts Title Case", () => {
+    const result = pascalize("Moz Transform");
+
+    expect(result).toBe(MozTransform);
+  });
+  it("removes leading and trailing hyphens", () => {
+    const result = pascalize("-moz-transform");
+
+    expect(result).toBe(MozTransform);
+  });
+  it("also removes leading and trailing non-alphanumeric characters", () => {
+    const result = pascalize("-_.*moz transform*._-");
+
+    expect(result).toBe(MozTransform);
+    // I'm so tired of writing this.
+  });
+  it("deals with numbers quite nicely", () => {
+    const result1 = pascalize("1thing2say3words");
+    const result2 = pascalize("ISS2houston"); // It's technically 3 words.
+    const result3 = pascalize("abc123");
+
+    expect(result1).toBe("1Thing2Say3Words");
+    expect(result2).toBe("ISS2Houston");
+
+    // This expectation shouldn't be different from the
+    // input since there's no non-alphabetic character
+    // preceeding an alphabetic character.
+    expect(result3).toBe(result3);
   });
 });
--- src/lib/pascalize/pascalize.ts
+++ src/lib/pascalize/pascalize.ts
@@ -1,3 +1,18 @@
-export const pascalize: (str: string) => string = (str) => {
-  return str;
-};
+import { capitalize } from "../capitalize";
+
+import { camscal } from "../../helpers";
+
+/**
+ * Camelize
+ *
+ * @export
+ * @param {string} str
+ * @returns {string}
+ *
+ * @description _Converts a string to PascalCase._
+ * @since v0.0.1
+ */
+
+export function pascalize(str: string): string {
+  return capitalize(camscal(str));
+}
